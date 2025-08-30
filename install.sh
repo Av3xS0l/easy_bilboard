fe6d339d4d7616ba9aa5b69981bf7fb1e21bdc43
Av3xS0l <62592319+Av3xS0l@users.noreply.github.com>
2025-08-30 23:42:52 +0300

    Add python3-pil to install dependencies

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 sudo apt update -y
-sudo apt install python3-pyqt5 python3-pyqt5.qtmultimedia libqt5multimedia5-plugins gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-base gstreamer1.0-libav gstreamer1.0-tools -y
+sudo apt install python3-pyqt5 python3-pyqt5.qtmultimedia python3-pil libqt5multimedia5-plugins gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-base gstreamer1.0-libav gstreamer1.0-tools -y
